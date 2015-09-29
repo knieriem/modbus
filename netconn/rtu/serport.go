43b4aed2acb44ad48f17f604ade30c9f30fccba5
Michael Teichgräber <mteichgraeber@gmx.de>
2015-09-29 14:01:43 +0200

    netconn/rtu: chooser impl moved to serport.Choose

--- netconn/rtu/serport.go
+++ netconn/rtu/serport.go
@@ -1,8 +1,6 @@
 package rtu
 
 import (
-	"errors"
-	"fmt"
 	"io"
 	"strings"
 
@@ -17,57 +15,12 @@ func portInfo(name string) string {
 }
 
 func openPort(cf *netconn.Conf) (c io.ReadWriteCloser, portName string, err error) {
-	var port serport.Port
-
-	portName = cf.Device
 	inictl := strings.Join(cf.Options, " ")
 
-	if portName != "" {
-		goto withName
-	}
-	portName, err = choosePort()
-	if err != nil {
-		return
-	}
-
-withName:
-	port, err = serport.Open(portName, inictl)
+	port, portName, err := serport.Choose(cf.Device, inictl)
 	if err == nil {
 		c = port
 	}
-	return
-}
-
-func choosePort() (name string, err error) {
-	sep := "-"
-	list := serenum.Ports()
-	switch len(list) {
-	case 0:
-		fmt.Print("Enter serial port: ")
-		_, err = fmt.Scanln(&name)
-		return
-	case 1:
-		name = list[0].Device
-		return
-	case 2:
-		sep = ", "
-	}
-	fmt.Println("\nChoose a serial port: ")
-	for i, p := range list {
-		fmt.Printf("  %d\t%v (%v)\n", i, p.Device, p.Format(nil))
-	}
-	fmt.Print("Enter a number (0", sep, len(list)-1, "): ")
-
-	var i int
-	_, err = fmt.Scanln(&i)
-	if err == nil {
-		if i < len(list) {
-			name = list[i].Device
-		} else {
-			err = errors.New("value exceeds maximum index")
-		}
-	}
-	fmt.Print("\n")
 	return
 }
 
